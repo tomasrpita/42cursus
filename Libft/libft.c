daf12980e1d5d52343ea8e84bd00e2a60b8e85c3
Tomás R. Pita <tomasrpita@gmail.com>
2019-11-05 07:04:57 +0100

    creacion de primeras funciones faciles

--- Libft/libft.c
+++ Libft/libft.c
@@ -1,4 +1,4 @@
-int	ft_strlen(char *s)
+int		ft_strlen(char *s)
 {
 	int	 i;
 	
@@ -6,4 +6,40 @@ int	ft_strlen(char *s)
 	while(s[i])
 		i++;
 	return (i);
+}
+
+
+int		ft_isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c <= 122))
+		return (1);
+	return (0);
+}
+
+int		ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int		ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+		return (1);
+	return (0);
+}
+
+int		ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
+
+int		ft_isprint(int c)
+{
+	if (c > 31 && c < 127)
+		return (1);
+	return (0);
 }
