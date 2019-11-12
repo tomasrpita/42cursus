f5671fa40107d1e45a24d4e3f6d13b3afc1e4a7c
tomasrpita <tomasrpita@gmail.com>
2019-11-12 14:43:05 +0100

    actualizacion header .h

--- Libft/libft.h
+++ Libft/libft.h
@@ -14,7 +14,19 @@
 #define LIBFT_H
 
 
-# include <stdlib.h>
+#include <stdlib.h>
 
+void *memset(void *s, int c, size_t n);
+void bzero(void *s, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memchr(const void *s, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+size_t strlen(const char *s);
+int isalpha(int c);
+int isdigit(int c);
+int isalnum(int c);
+int isascii(int c);
+int isprint(int c);
 
 #endif
