878b1d61bd554b93afbf20b1a8dc302eee46d97f
Tomas Pita de sousa <tpita-de@c3r10s2.42madrid.com>
2019-11-23 14:58:48 +0100

    agregado ft_split.c, funcional y probado

--- Libft/tpita-de/ft_split.c
+++ Libft/tpita-de/ft_split.c
@@ -6,119 +6,75 @@
 /*   By: tpita-de <tpita-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 13:39:36 by tpita-de          #+#    #+#             */
-/*   Updated: 2019/11/21 21:58:40 by tpita-de         ###   ########.fr       */
+/*   Updated: 2019/11/23 14:54:52 by tpita-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-static char    **ft_crtmtx(size_t row, size_t col)
+static size_t	ft_count_words(char const *s, char c)
 {
-    size_t        i;
-    size_t        j;
-    char        **matrix;
+	size_t	i;
+	size_t	words;
 
-    if (row < 0 || col < 0)
-        return (NULL);
-    if ((matrix = (char **)malloc((row + 1) * sizeof(char *))) == NULL)
-        return (NULL);
-    i = 0;
-    while (i < row)
-    {
-        if ((matrix[i] = (char *)malloc((col + 1)* sizeof(char))) == NULL)
-            return (NULL);
-        j = 0;
-        while (j <= col)
-        {
-            matrix[i][j] = 0;
-            ++j;
-        }
-        
-        ++i;
-    }
-    matrix[row] = 0;
-    return (matrix);
+	i = 0;
+	words = 0;
+	if (s[i] == c)
+		++words;
+	++i;
+	while (s[i])
+	{
+
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != 0)
+			++words;
+		++i;
+	}
+	return (words);
 }
 
-char    **ft_split(char const *s, char c)
+static char	*ft_malloc_word(char const *s, char c)
 {
-    size_t        i;
-    size_t        row;
-    size_t        col;
-    size_t        count;
-    char        **matrix;
-    size_t        j;
-    size_t        k;
-    size_t        l;
-    size_t        flag;
+	char	*word;
+	int		i;
 
-    i = 0;
-    row = 0;
-    if (s[i] == c)
-        ++row;
-    ++i;
-    while (s[i])
-    {
-
-        if (s[i] == c && s[i + 1] != c && s[i + 1] != 0)
-            ++row;
-        ++i;
-    }
-    i = 0;
-    col = 0;
-    j = -1;
-    while (s[i])
-    {
-        ++j;
-        count = 0;
-        while (s[j] != c && s[j])
-        {
-          ++count;
-          ++j;
-        }
-        if (count > col)
-          col = count;
-        i = j;
-    }
-    matrix = ft_crtmtx(row, col);
-
-    i = 0;
-    col = 0;
-    j = -1;
-    k = 0;
-    l = 0;
-    
-    while(matrix[k])
-    {
-        while (s[i])
-        {
-            ++j;
-            count = 0;
-            flag = 0;
-            while (s[j] != c && s[j])
-            {
-                matrix[k][l] = s[j];
-                ++l;
-                ++j;
-                ++flag; 
-            }
-            l = 0;
-            i = j;
-            if (flag != 0)
-              
-            if (matrix[k] == 0)
-                return (matrix);
-            ++k;
-        }
-        
-    }
-    return (NULL);
+	i = 0;
+	while (s[i] && s[i] != c)
+		++i;
+	word = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		word[i] = s[i];
+		++i;
+	}
+	word[i] = '\0';
+	return (word);
 }
 
-int main()
+char		**ft_split(char const *s, char c)
 {
-  char **retorno;
-  
-  ft_split("*Hola*mi*amigo*como*estas*" , '*');
-  return (0);
+	size_t	words;
+	char	**matrix;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	words = ft_count_words(s, c);
+	if ((matrix = (char **)malloc((words + 1) * sizeof(char *))) == NULL)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			++s;
+		if (*s && *s != c)
+		{
+			matrix[i] = ft_malloc_word(s, c);
+			++i;
+			while (*s && *s != c)
+				++s;
+		}
+	}
+	matrix[i] = NULL;
+	return (matrix);
 }
