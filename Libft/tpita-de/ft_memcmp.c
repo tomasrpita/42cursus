2beb9cd11a5f51ca67953b669f3989c969458882
Tomas Pita de sousa <tpita-de@c3r14s6.42madrid.com>
2019-11-29 21:14:15 +0100

    subo archivos de prueba

--- Libft/tpita-de/ft_memcmp.c
+++ Libft/tpita-de/ft_memcmp.c
@@ -6,12 +6,11 @@
 /*   By: tpita-de <tpita-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 16:41:06 by tpita-de          #+#    #+#             */
-/*   Updated: 2019/11/29 18:26:57 by tpita-de         ###   ########.fr       */
+/*   Updated: 2019/11/29 19:59:55 by tpita-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -20,8 +19,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	t_str1 = (unsigned const char *)s1;
 	s_str2 = (unsigned const char *)s2;
-	if (*t_str1 == '\0' || *s_str2 == '\0')
-		return (0);
 	while (n)
 	{
 		if (*t_str1 != *s_str2)
