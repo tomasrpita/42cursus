d2d5ccc974d099143d39830fb5e9177deb5c67db
Tomas Pita de sousa <tpita-de@c2r8s6.42madrid.com>
2019-12-29 17:29:01 +0100

    agrega comentarios a ft_memset, ft_bzero y ft_mrmcpy

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,11 +6,26 @@
 /*   By: tpita-de <tpita-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 17:37:10 by tpita-de          #+#    #+#             */
-/*   Updated: 2019/11/14 21:11:49 by tpita-de         ###   ########.fr       */
+/*   Updated: 2019/12/22 15:04:30 by tpita-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* ************************************************************************** */
+/*                                                                            */
+/* DESCRIPCION																  */
+/* 		borra los datos de la memoria por el tamaño de n comenzando en el *s  */
+/*		escribiendo ceros (bytes '\0') en esa area. 						  */
+/*												                              */
+/* PARAMETROS																  */
+/* 		void s: donde se comienza a borrar la memoria  	     				  */
+/* 		size_t 	n:  el tamaño de la memoria que sera borrada				  */
+/* 												                              */
+/* VALOR DE RETORNO 														  */
+/* 		nada, void                          								  */
+/*												                              */
+/* ************************************************************************** */
 
 void	ft_bzero(void *s, size_t n)
 {
--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,11 +6,27 @@
 /*   By: tpita-de <tpita-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 17:24:43 by tpita-de          #+#    #+#             */
-/*   Updated: 2019/11/14 20:59:08 by tpita-de         ###   ########.fr       */
+/*   Updated: 2019/12/22 14:57:05 by tpita-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* ************************************************************************** */
+/*                                                                            */
+/* DESCRIPCION																  */
+/* 		Escribe el valor de c (convertido a unsigned char) la cantidd de 	  */
+/*		veces dadas por len en los primeros bytes de  b						  */
+/*												                              */
+/* PARAMETROS																  */
+/* 		void b: donde se va a copiar los caracteres  	     				  */
+/* 		int	c:  es un entero con el valor de un caracter					  */
+/* 		size_t 	len:  el numero de veces que se copiara c					  */
+/* 												                              */
+/* VALOR DE RETORNO 														  */
+/* 		Retorna b, o sea un puntero void									  */
+/*												                              */
+/* ************************************************************************** */
 
 void	*ft_memset(void *b, int c, size_t len)
 {
